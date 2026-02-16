7f29c949d2ab233bc40eee34c034f4d2e0291a8d
Nikita Morozov <nikitwww@gmail.com>
2026-02-16 17:15:44 +0300

    ref: flake

--- flake.nix
+++ flake.nix
@@ -71,7 +71,6 @@
           }
           ./system
           ./hosts/${hostname}/configuration.nix
-          ./hosts/${hostname}/hardware.nix
           home-manager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
