b2444fda79573aa34c863711c9239704be9fe802
Morozov Nikita <nikitwww@gmail.com>
2026-06-11 00:23:07 +0300

    fix(frp): update syntax

--- hosts/orangepi/frpc.nix
+++ hosts/orangepi/frpc.nix
@@ -3,38 +3,40 @@
 
 {
   services.frp = {
-    enable = true;
-    role = "client";
+    instances."" = {
+      enable = true;
+      role = "client";
 
-    settings = {
-      serverAddr = "159.194.214.57";
-      serverPort = 7000;
+      settings = {
+        serverAddr = "159.194.214.57";
+        serverPort = 7000;             #
 
-      auth = {
-        method = "token";
-        token = "AHAHAHAH_SOSAL?!";
+        auth = {
+          method = "token";
+          token = "AHAHAHAH_SOSAL?!";
+        };
+
+        transport = {
+          protocol = "quic";
+        };
+
+        proxies = [
+          {
+            name = "minecraft-tcp";
+            type = "tcp";
+            localIP = "127.0.0.1";
+            localPort = 25565;
+            remotePort = 25565;
+          }
+          {
+            name = "minecraft-udp";
+            type = "udp";
+            localIP = "127.0.0.1";
+            localPort = 25564;
+            remotePort = 24454;
+          }
+        ];
       };
-
-      transport = {
-        protocol = "quic";
-      };
-
-      proxies = [
-        {
-          name = "minecraft-tcp";
-          type = "tcp";
-          localIP = "127.0.0.1";
-          localPort = 25565;
-          remotePort = 25565;
-        }
-        {
-          name = "minecraft-udp";
-          type = "udp";
-          localIP = "127.0.0.1";
-          localPort = 25564;
-          remotePort = 24454;
-        }
-      ];
     };
   };
 }
