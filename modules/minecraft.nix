8deccfe871a8f980886dd60fdc9785ec07501127
Morozov Nikita <nikitwww@gmail.com>
2026-06-11 01:26:27 +0300

    feat(mc): add mcrcon and automated daily backups

--- modules/minecraft.nix
+++ modules/minecraft.nix
@@ -20,6 +20,8 @@
 
       networking.nameservers = [ "8.8.8.8" "1.1.1.1" ];
 
+      environment.systemPackages = [ pkgs.mcrcon ];
+
       users.users.minecraft = {
         isSystemUser = true;
         group = "minecraft";
@@ -70,6 +72,36 @@
 
           Restart = "always";
           TimeoutStopSec = "60";
+        };
+      };
+
+      systemd.services.minecraft-backup = {
+        description = "Minecraft World Backup Service";
+        serviceConfig = {
+          Type = "oneshot";
+          User = "minecraft";
+          Group = "minecraft";
+        };
+        script = ''
+          BACKUP_DIR="/var/lib/minecraft/backups"
+          mkdir -p "$BACKUP_DIR"
+
+          DATE=$(${pkgs.coreutils}/bin/date +%Y-%m-%d_%H-%M-%S)
+          echo "Starting backup of minecraft world..."
+          ${pkgs.gnutar}/bin/tar -czf "$BACKUP_DIR/world_$DATE.tar.gz" -C /var/lib/minecraft world
+          echo "Backup created: world_$DATE.tar.gz"
+
+          ${pkgs.findutils}/bin/find "$BACKUP_DIR" -name "world_*.tar.gz" -mtime +7 -delete
+          echo "Old backups cleaned up."
+        '';
+      };
+
+      systemd.timers.minecraft-backup = {
+        description = "Timer for Minecraft World Backup";
+        wantedBy = [ "timers.target" ];
+        timerConfig = {
+          OnCalendar = "04:00:00";
+          Persistent = true;
         };
       };
     };
