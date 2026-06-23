ed054c9a745a0a86f78a43aefca43eb134fa5abf
Morozov Nikita <nikitwww@gmail.com>
2026-06-23 17:51:35 +0300

    ssh: remove password auth

--- modules/system/server/sshd.nix
+++ modules/system/server/sshd.nix
@@ -5,7 +5,7 @@
     ports = [ 7843 ];
     settings = {
       X11Forwarding = lib.mkDefault true;
-      PasswordAuthentication = lib.mkDefault true;
+      PasswordAuthentication = lib.mkDefault false;
       AuthorizedKeysFile = "%h/.ssh/authorized_keys /run/secrets/user/ssh_key";
     };
     openFirewall = lib.mkDefault true;
